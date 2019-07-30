82041161bdd37cc4b916b9b9d137fcc32351b7a1
khuynhthanh <phamanhtuan1406@gmail.com>
2019-07-30 10:04:41 +0700

    tao interface va update service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {IApp} from './app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
+  private readonly API_URL = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getPosts(count = 10): Observable<IApp[]> {
+    return this.http.get<IApp[]>(this.API_URL);
+  }
+
+  getPostById(id: number): Observable<IApp> {
+    return this.http.get<IApp>(`${this.API_URL}/${id}`);
+  }
 }
---
+++ src/app/app.ts
@@ -0,0 +1,7 @@
+export interface IApp {
+  firstName: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  password: string;
+}
