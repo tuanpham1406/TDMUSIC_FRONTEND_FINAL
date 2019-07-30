28c0205f6f021d8c655151535ac6ccc5fa9fdaf4
khuynhthanh <phamanhtuan1406@gmail.com>
2019-07-30 09:57:48 +0700

    create component login and register

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,10 +6,13 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FooterComponent} from './footer/footer.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, NgbModule],
-  declarations: [FooterComponent, NavbarComponent, SidebarComponent],
+  imports: [CommonModule, RouterModule, NgbModule, ReactiveFormsModule],
+  declarations: [FooterComponent, NavbarComponent, SidebarComponent, LoginComponent, RegisterComponent],
   exports: [FooterComponent, NavbarComponent, SidebarComponent]
 })
 export class ComponentsModule {
